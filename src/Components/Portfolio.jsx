abad2cfa049bd3fd56c4cc7e26ca2c5840529d5f
Akash <155997340+AkashMunnangi@users.noreply.github.com>
2024-06-15 13:24:35 +0000

    updated portfolio

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,29 +31,20 @@ const imageAltText = "Akash explaing his project in a school";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Digi Locker",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "I went to a school and explaind about DigiLocker community service project",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Virtual ID",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "I went to a school and explaind about Virtual ID as part of community service project",
   },
   {
-    title: "My Resume Site",
+    title: "App developed",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
-  },
+      "I have developed a app about the expiry dates which is used to remind us expired tablets,warrenty,etc..",
+  }
 ];
 
 const Portfolio = () => {

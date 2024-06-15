30a2d381fe926aae4cb460b37af434b9160cbc19
Akash <155997340+AkashMunnangi@users.noreply.github.com>
2024-06-15 13:16:03 +0000

    updtated about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,23 @@ const imageAltText = "SIMPLE background image";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am Akash Munnangi, CR Class Representative of my class I used to help my teachers and students";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
+  "JAVA DEVELOPER",
+  "PYTHON",
+  "HTML",
+  "CSS",
   "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "PROBLEM SOLVING",
+  "COMMUNICATION",
+  "ANALYSIS",
+  "TIME MANAGEMENT",
+  "ADAPTABLE"
 ];
 
 /**
@@ -45,8 +49,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
-
+  "I am eager to build my career";
 const About = () => {
   return (
     <section className="padding" id="about">
